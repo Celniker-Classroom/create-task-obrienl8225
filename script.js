95af856517c0b6b250fff4bfbda25a54732fbe22
obrienl8225 <z2chy67qhf@privaterelay.appleid.com>
2026-04-15 04:44:43 +0000

    added javascript for each radio button; fixed radio buttons for toppings so the user could select multiple toppings; also added calculatePrice function as well as assigned different prices for each part of the pizza

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // add javascript here
+const sizePrices = { 
+    small: 5,
+    medium: 10,
+    large: 15
+};
+
+const crustPrices = {
+    thin: 0,
+    stuffed: 2,
+    "gluten-free": 3
+};
+
+const toppingPrices = {
+    pepperoni: 1,
+    mushrooms: 0.5,
+    onions: 0.5,
+    olives: .5,
+    "bell-peppers": .5,
+    pineapple: 1
+};
+
+function calculatePrice(size, crust, toppings) {
+    let total = 0;
+
+    total += sizePrices[size];
+    total += crustPrices[crust];
+
+    for(let i = 0; i < toppingPrices.length; i++) {
+        total += toppingPrices[toppings[i]];
+    }
+
+    return total;
+}
+
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("change", updatePizza);
+});
